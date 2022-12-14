d933fce4d1c279379e93c84de30e5c8b9defc58b
Adam3192 <jeremyadixon@hotmail.com>
2022-12-14 15:13:52 -0600

    Get current user liked songs

--- backEnd/src/controllers/songController.ts
+++ backEnd/src/controllers/songController.ts
@@ -1,17 +1,23 @@
 import { RequestHandler } from 'express'
-import { Message } from '../models/message'
 import { Song } from '../models/song'
-import { User } from '../models/user'
-import { verifyUser } from '../services/auth'
 
 export const createSong: RequestHandler = async (req, res, next) => {
- let newSong: Song = req.body
- // newSong.userId = user.userId
+  let newSong: Song = req.body
 
- if (newSong.title && newSong.artist) {
-   let created = await Song.create(newSong)
-   res.status(201).json(created)
- } else {
-   res.status(400).send()
- }
+  if (newSong.title && newSong.artist) {
+    let created = await Song.create(newSong)
+    res.status(201).json(created)
+  } else {
+    res.status(400).send()
+  }
+}
+
+export const currentUserSongs: RequestHandler = async (req, res, next) => {
+  let username = req.params.username
+
+  let songs = await Song.findAll({
+    where: { username: username },
+  }).then((response) => {
+    res.status(200).json(response)
+  })
 }
