a385e70387914e1f175c4c50a3bcee087897a91a
Kyla Joiner <kylajoiner@yahoo.com>
2023-01-10 19:13:12 -0700

    updated userController and routes to match dev branch

--- backEnd/src/controllers/userController.ts
+++ backEnd/src/controllers/userController.ts
@@ -1,28 +1,51 @@
 import { RequestHandler } from 'express'
 import { User } from '../models/user'
-import {
-  comparePasswords,
-  hashPassword,
-  signUserToken,
-  verifyUser,
-} from '../services/auth'
+const SpotifyWebApi = require('spotify-web-api-node')
 
-export const createUser: RequestHandler = async (req, res, next) => {
-  let newUser: User = req.body
-  if (newUser.userId && newUser.email && newUser.display_name) {
-    try {
-      let created = await User.create(newUser)
-      res.status(201).json({
-        username: created.userId,
-        email: created.email,
-        display_name: created.display_name
-      })
-    } catch (error) {
-      console.log(error)
+const spotifyApi = new SpotifyWebApi({
+  clientId: '69372f48d4b24c099e581c69793c1879',
+})
+
+export const currentUser: RequestHandler = async (req, res, next) => {
+  let token = req.headers.authorization?.split(' ')[1];
+
+  spotifyApi.setAccessToken(token)
+
+  spotifyApi.getMe().then(
+    async function (data: any) {
+
+      let image;
+
+      if(data.body.images[0]) {
+        image = data.body.images[0].url
+      } else {
+        console.log('image not found')
+      }
+
+      let newUser: any = {
+        userId: data.body.id,
+        email: data.body.email,
+        display_name: data.body.display_name,
+        profilePic: image
+      }
+
+      if (newUser.userId && newUser.email && newUser.display_name) {
+        try {
+          let created = await User.create(newUser);
+          return created;
+        } catch (error) {
+          console.log(error)
+        }
+      } else {
+        console.log(`userId, email, and display_name required`)
+      }
+
+      res.status(200).json(data.body)
+    },
+    function (err: any) {
+      console.log('Something went wrong!', err)
     }
-  } else {
-    res.status(400).send('UserId, email, and display_name required')
-  }
+  )
 }
 
 export const getAllUsers: RequestHandler = async (req, res, next) => {
