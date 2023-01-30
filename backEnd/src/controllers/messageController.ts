657acbf15add48f69a1ab2551fd4551a5a1b0703
Kyla Joiner <kylajoiner@yahoo.com>
2023-01-29 21:50:07 -0700

    U in CRUD messages

--- backEnd/src/controllers/messageController.ts
+++ backEnd/src/controllers/messageController.ts
@@ -35,6 +35,36 @@ export const createMessage: RequestHandler = async (req, res, next) => {
   }
 }
 
+export const editMessage: RequestHandler = async (req, res, next) => {
+  let user: User | null = await verifyUser(req, res, next);
+
+  if (!user) {
+    return res.status(403).send('User not detected');
+  }
+
+  let messageId = req.params.messageId;
+  let currentUserId = user.userId;
+  console.log(req.body);
+
+  let updatedMessage: Message = req.body;
+  let messageFound = await Message.findByPk(messageId);
+
+  if (messageFound && messageFound.messageId && messageFound.userId === currentUserId) {
+    await Message.update(updatedMessage, {
+      where: { 
+        messageId: messageId,
+        userId: currentUserId
+      }
+    });
+    res.status(200).json('Updated message is a success!');
+    console.log('Updated message is a success in the backend!')
+  }
+  else {
+    res.status(400).json('An error occured while updating this message');
+    console.log('An error occured while updating this message in the backend')
+  }
+}
+
 export const deleteMessage: RequestHandler = async (req, res, next) => {
   let user: User | null = await verifyUser(req, res, next);
 
@@ -56,8 +86,10 @@ export const deleteMessage: RequestHandler = async (req, res, next) => {
       }
     });
     res.status(200).json('Message deleted!');
+    console.log('Message deleted! -backend');
   }
   else {
     res.status(404).json('An error occured while deleting this message');
+    console.log('An error occured while deleting this message -backend');
   }
 }
--- backEnd/src/routes/messageRoutes.ts
+++ backEnd/src/routes/messageRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createMessage, deleteMessage, getAllMessages, getOneMessage } from '../controllers/messageController';
+import { createMessage, deleteMessage, editMessage, getAllMessages, getOneMessage } from '../controllers/messageController';
 
 const router = Router();
 
 router.get('/', getAllMessages);
 router.get('/:messageId', getOneMessage);
 router.post('/', createMessage);
+router.put('/:messageId', editMessage);
 router.delete('/:messageId', deleteMessage);
 
 export default router;
